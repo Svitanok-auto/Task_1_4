de0298587c6a79d30ee1c9f80c879bb939d3c261
Sveta <bogatovasvetlanau@gmail.com>
2021-04-08 19:49:45 +0300

    Added input special characters validation

--- Practice4/Program.cs
+++ Practice4/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Practice4
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public const int MAX_ATTEMPTS = 10;
+        public static void Main(string[] args)
         {
+            CheckIfStringIsAPalindromeInLoop();
+            Console.ReadKey();
+        }
+
+        private static void CheckIfInputStringIsAPalindrome(char []array)
+        {
+            int i = 0;
+            while ((array[i] == array[array.Length - i - 1]) && (i < (array.Length - 1) / 2) && (array.Length > 2) && (array.Length % 2 != 0))
+            {
+                i++;
+            }
+            if (i == (((array.Length - 1) / 2)) && (array.Length > 2) && (array.Length % 2 != 0))
+            {
+                Console.WriteLine("\nThe word is a Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("\nThe word is NOT a Palindrome");
+            }
+        }
+
+        private static void CheckIfStringIsAPalindromeInLoop()
+        {
+            int attempt = 0;
+            while (attempt < MAX_ATTEMPTS)
+            {
+                CheckIfInputStringIsAPalindrome(Validator.GetValidatedCharsArray());
+            }
         }
     }
 }
